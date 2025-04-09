2ded69ceb67923662e84edd5453a647aca67e3a6
kooswerkhoven <koos1610werkhoven@gmail.com>
2025-04-09 11:24:38 +0200

    menu update
    fotos geadd in de code

--- scripts/config.js
+++ scripts/config.js
@@ -26,34 +26,34 @@ let autoEpcInterval = null;
 //autoclicker settings
 let typefoods ={
     Zwerver: [
-        { name: "Vuilnisbak", price: 0, tier: "Zwerver", multiplier: 1, foto: "fotos/afvalcontainer-removebg-preview.png" },
+        { name: "Vuilnisbak", price: 0, tier: "Zwerver", multiplier: 1, foto: "fotos/afvalcontainer-removebg-preview.png"},
         { name: "Stoep Kauwgom", price: 1000, tier: "Zwerver", multiplier: 2, foto: "fotos/streetgun.png" },
-        { name: "Bananen Schil", price: 3500, tier: "Zwerver", multiplier: 3, foto: "banana-peel.png" },
-        { name: "Rat", price: 10000, tier: "Zwerver", multiplier: 5 },
+        { name: "Bananen Schil", price: 3500, tier: "Zwerver", multiplier: 3, foto: "fotos/banana-peel.png" },
+        { name: "Rat", price: 10000, tier: "Zwerver", multiplier: 5, foto:"fotos/rat.png" },
       ],
       Arm: [
-        { name: "Bruine Banaan", price: 0, tier: "Zwerver", multiplier: 1 },
-        { name: "Rijst", price: 1000, tier: "Zwerver", multiplier: 2 },
-        { name: "Aardappel", price: 3500, tier: "Zwerver", multiplier: 3 },
-        { name: "Brood", price: 10000, tier: "Zwerver", multiplier: 5 },
+        { name: "Bruine Banaan", price: 0, tier: "Zwerver", multiplier: 1, foto:"fotos/brown-banana.png" },
+        { name: "Rijst", price: 1000, tier: "Zwerver", multiplier: 2, foto:"fotos/rijst.png" },
+        { name: "Aardappel", price: 3500, tier: "Zwerver", multiplier: 3, foto:"fotos/aardappelnew.png" },
+        { name: "Brood", price: 10000, tier: "Zwerver", multiplier: 5, foto:"fotos/bread.png" },
       ],
       Normaal: [
-        { name: "Banaan", price: 0, tier: "Zwerver", multiplier: 1 },
-        { name: "Burger", price: 1000, tier: "Zwerver", multiplier: 2 },
-        { name: "Spaghetti", price: 3500, tier: "Zwerver", multiplier: 3 },
+        { name: "Banaan", price: 0, tier: "Zwerver", multiplier: 1, foto:"fotos/banaan.png" },
+        { name: "Burger", price: 1000, tier: "Zwerver", multiplier: 2, foto:"fotos/burger.png" },
+        { name: "Spaghetti", price: 3500, tier: "Zwerver", multiplier: 3, foto:"fotos/spagghetti.png" },
       ],
       Rijk: [
-        { name: "Sushi", price: 0, tier: "Zwerver", multiplier: 1 },
-        { name: "Bananensplit", price: 1000, tier: "Zwerver", multiplier: 2 },
-        { name: "Kreeft", price: 3500, tier: "Zwerver", multiplier: 3 },
-        { name: "Caviar", price: 10000, tier: "Zwerver", multiplier: 5 },
-        { name: "Wagyu Steak", price: 10000, tier: "Zwerver", multiplier: 5 },
+        { name: "Sushi", price: 0, tier: "Zwerver", multiplier: 1, foto:"fotos/sushi.png" },
+        { name: "Bananensplit", price: 1000, tier: "Zwerver", multiplier: 2, foto:"fotos/bananensplit.png" },
+        { name: "Kreeft", price: 3500, tier: "Zwerver", multiplier: 3, foto:"fotos/lobster.png" },
+        { name: "Caviar", price: 10000, tier: "Zwerver", multiplier: 5, foto:"fotos/caviar.png" },
+        { name: "Wagyu Steak", price: 10000, tier: "Zwerver", multiplier: 5, foto:"fotos/wagyu-steak.png"},
       ],
       Legend: [
-        { name: "Gouden pizza", price: 0, tier: "Zwerver", multiplier: 1 },
-        { name: "Dodo", price: 1000, tier: "Zwerver", multiplier: 2 },
-        { name: "Uranium", price: 3500, tier: "Zwerver", multiplier: 3 },
-        { name: "Supreme Banaan", price: 10000, tier: "Zwerver", multiplier: 5 },
+        { name: "Gouden pizza", price: 0, tier: "Zwerver", multiplier: 1, foto:"fotos/goldenpizza.png" },
+        { name: "Dodo", price: 1000, tier: "Zwerver", multiplier: 2, foto:"fotos/cartoon-dodo.png" },
+        { name: "Uranium", price: 3500, tier: "Zwerver", multiplier: 3, foto:"fotos/uranium.png" },
+        { name: "Supreme Banaan", price: 10000, tier: "Zwerver", multiplier: 5, foto:"fotos/supreme-vanana.png" },
       ],
 }
 //FOODS/TIERS
