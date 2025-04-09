1dac95e4cf91200e5ba44c58d87261f3eb374a45
G-M-PH <GiaminhPham@outlook.com>
2025-04-09 11:11:17 +0200

    Food
    Geadd:
    Objects voor food
    Design
    tiers objects

--- scripts/config.js
+++ scripts/config.js
@@ -24,6 +24,39 @@ let autoclickpower = 0
 let autoclickspeed = 1
 let autoEpcInterval = null;
 //autoclicker settings
+let typefoods ={
+    Zwerver: [
+        { name: "Vuilnisbak", price: 0, tier: "Zwerver", multiplier: 1, foto: "fotos/afvalcontainer-removebg-preview.png" },
+        { name: "Stoep Kauwgom", price: 1000, tier: "Zwerver", multiplier: 2, foto: "fotos/streetgun.png" },
+        { name: "Bananen Schil", price: 3500, tier: "Zwerver", multiplier: 3, foto: "banana-peel.png" },
+        { name: "Rat", price: 10000, tier: "Zwerver", multiplier: 5 },
+      ],
+      Arm: [
+        { name: "Bruine Banaan", price: 0, tier: "Zwerver", multiplier: 1 },
+        { name: "Rijst", price: 1000, tier: "Zwerver", multiplier: 2 },
+        { name: "Aardappel", price: 3500, tier: "Zwerver", multiplier: 3 },
+        { name: "Brood", price: 10000, tier: "Zwerver", multiplier: 5 },
+      ],
+      Normaal: [
+        { name: "Banaan", price: 0, tier: "Zwerver", multiplier: 1 },
+        { name: "Burger", price: 1000, tier: "Zwerver", multiplier: 2 },
+        { name: "Spaghetti", price: 3500, tier: "Zwerver", multiplier: 3 },
+      ],
+      Rijk: [
+        { name: "Sushi", price: 0, tier: "Zwerver", multiplier: 1 },
+        { name: "Bananensplit", price: 1000, tier: "Zwerver", multiplier: 2 },
+        { name: "Kreeft", price: 3500, tier: "Zwerver", multiplier: 3 },
+        { name: "Caviar", price: 10000, tier: "Zwerver", multiplier: 5 },
+        { name: "Wagyu Steak", price: 10000, tier: "Zwerver", multiplier: 5 },
+      ],
+      Legend: [
+        { name: "Gouden pizza", price: 0, tier: "Zwerver", multiplier: 1 },
+        { name: "Dodo", price: 1000, tier: "Zwerver", multiplier: 2 },
+        { name: "Uranium", price: 3500, tier: "Zwerver", multiplier: 3 },
+        { name: "Supreme Banaan", price: 10000, tier: "Zwerver", multiplier: 5 },
+      ],
+}
+//FOODS/TIERS
 function formatMoney(amount) { //Formatting voor getallen
     if (amount >= 1e12) {
         return (amount / 1e2).toFixed(1) + 'T'; //triljoen
